2981f38bf6f7ca7ddec141f21725076b6980fa77
sidharthghai4242 <sidharthghai48@gmail.com>
2024-07-02 18:30:44 +0530

    sign in functionality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link,useNavigate } from 'react-router-dom';
 
 export default function Signin() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate =useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);  // Reset error state on submit
+
+    try {
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      console.log('Response status:', res.status);
+
+      if (!res.ok) {
+        const errorData = await res.json();
+        throw new Error(errorData.message || 'Network response was not ok');
+      }
+
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+
+      setLoading(false);
+      setError(null);
+      navigate('/');
+      // console.log('Response data:', data);
+
+      // Handle successful response (if needed)
+
+    } catch (error) {
+      setLoading(false);
+      console.error('Error:', error);
+      setError(error.message);  // Set error message
+    }
+  };
+
+  console.log(formData);
   return (
-    <div>
-      Sign-In
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign-in</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
+        <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
+        <button type='submit' disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          {loading ? 'Loading...' : 'Sign-in'}
+        </button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Dont Have an account?</p>
+        <Link to={"/sign-up"}>
+          <span className='text-blue-700'>Sign-up</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
-  )
+  );
 }
