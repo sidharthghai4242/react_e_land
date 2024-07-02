1bd6136881e23bc018a8758c87413c09d0c78fea
sidharthghai4242 <sidharthghai48@gmail.com>
2024-07-02 22:45:29 +0530

    redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,14 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from'./user/userSlice';
+import {persistReducer,persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+// import persistStore from 'redux-persist/es/persistStore';
+
+const rootReducer =combineReducers({user:userReducer})
+const persistConfig={
+  key:'root',
+  storage,
+  version:1,
+}
+
+const persistedReducer=persistReducer(persistConfig,rootReducer)
+
+
 
 export const store = configureStore({
-  reducer: {user:userReducer},
+  reducer: persistedReducer,
   middleware:(getDefaultMiddleware)=> getDefaultMiddleware({
     serializableCheck:false,
   }),
 });
 
-// // Infer the `RootState` and `AppDispatch` types from the store itself
-// export type RootState = ReturnType<typeof store.getState>
-// // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-// export type AppDispatch = typeof store.dispatch
+export const persistor=persistStore(store);
