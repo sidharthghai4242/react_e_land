baaa8db1f89d1db2771a253589364692442911cf
sidharthghai4242 <sidharthghai48@gmail.com>
2024-07-09 16:25:15 +0530

    user controller

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import cors from 'cors'; // Import cors package
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -25,7 +25,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
-
+app.use(cookieParser());
 app.listen(3000, () => {
   console.log('Server is running on port 3000 !!!');
 });
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -18,7 +18,7 @@ export default function Profile() {
   },[file]);
   const handleFileUpload=(file)=>{
     const storage = getStorage(app);
-    const fileName =new Date().getTime+file.name;
+    const fileName =new Date().getTime()+file.name;
     const storageRef=ref(storage,fileName);
     const uploadTask =uploadBytesResumable(storageRef,file);
     uploadTask.on('state_changed',
@@ -26,16 +26,16 @@ export default function Profile() {
         const progress=(snapshot.bytesTransferred/snapshot.totalBytes)*100;
         setFilePerc(Math.round(progress));
       },
-    
     (error)=>{
       setFileUploadError(true);
     },
     ()=>{
-      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
-          setFormData({...formData,avatar:downloadURL});
-      })
+      getDownloadURL(uploadTask.snapshot.ref).then
+      ((downloadURL)=>
+          setFormData({...formData,avatar:downloadURL})
+      );
     }
-  );
+   );
   };
   return (
     <div className='p-3 max-w-lg mx-auto'>
